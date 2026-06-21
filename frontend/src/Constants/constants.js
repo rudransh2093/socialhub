bab22d95443c6a6673173700347445eee52b3428
Rudransh Pillay <pillay.rudransh2@gmail.com>
2026-06-21 19:27:57 +0530

    Update production API URL in constants.js to match active Render service

--- frontend/src/Constants/constants.js
+++ frontend/src/Constants/constants.js
@@ -1,4 +1,4 @@
 export const SERVER_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? "http://127.0.0.1:8000/api"
-  : "https://socialhub.onrender.com/api";
+  : "https://social-media-django-react.onrender.com/api";
 
